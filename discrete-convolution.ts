5ca75eab567dfecb37fdb0a79c7389636169d2f6
Dirk Arnez <smalldirkalex@gmail.com>
2023-12-16 22:56:26 +0800

    - add discrete-convolution.ts

---
+++ discrete-convolution.ts
@@ -0,0 +1,16 @@
+function discreteConvolution(signal: number[], kernel: number[]): number[] {
+  const signalLength = signal.length;
+  const kernelLength = kernel.length;
+  const resultLength = signalLength + kernelLength - 1;
+  const result: number[] = Array(resultLength).fill(0);
+
+  for (let i = 0; i < signalLength; i++) {
+    for (let j = 0; j < kernelLength; j++) {
+      result[i + j] += signal[i] * kernel[j];
+    }
+  }
+
+  return result;
+}
+
+console.log(discreteConvolution([1, 2, 3], [4, 5, 6]));
